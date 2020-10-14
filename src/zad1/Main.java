f71437b7da0fdaad4a16a233ffc8859bc534f364
versm <s18991@pjwstk.edu.pl>
2020-10-14 23:22:43 +0200

    .

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -15,7 +15,7 @@ import java.util.concurrent.*;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    String testFileName = System.getProperty("user.home") + "/ChatTest.txt";
+    String testFileName = "ChatTest.txt";
     List<String> test = Files.readAllLines(Paths.get(testFileName));
     String host = test.remove(0);
     int port = Integer.valueOf(test.remove(0));
